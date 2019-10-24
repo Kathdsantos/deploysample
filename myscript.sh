ee002b5eda8104d8a023f64efbd14e09adb86a34
chasemarellan <56814589+chasemarellan@users.noreply.github.com>
2019-10-23 23:01:16 -0400

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -14,7 +14,7 @@ echo "COPY  ./static /home/ubuntu/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/ubuntu/templates/" >> tempdir/Dockerfile
 echo "COPY  sample_app.py /home/ubuntu/" >> tempdir/Dockerfile
 echo "EXPOSE 80" >> tempdir/Dockerfile
-echo “CMD python /home/ubuntu/sample_app.py” >> tempdir/Dockerfile
+echo "CMD python /home/ubuntu/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
 sudo docker build -t sampleapp .
