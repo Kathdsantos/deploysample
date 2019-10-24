6eb72da4313624647cc773828e17392ee4685610
chasemarellan <56814589+chasemarellan@users.noreply.github.com>
2019-10-23 22:46:12 -0400

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,6 +1,3 @@
-rm -r tempdir/*
-rmdir tempdir
-
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
@@ -11,14 +8,16 @@ cp -r static/* tempdir/static/.
 
 echo "FROM python" > tempdir/Dockerfile
 echo "RUN pip install flask" >> tempdir/Dockerfile
-echo "COPY ./* /home/ubuntu/" >> tempdir/Dockerfile
-echo "RUN python /home/ubuntu/sample_app.py &" >> tempdir/Dockerfile
+echo "COPY  ./static /home/ubuntu/static/" >> tempdir/Dockerfile
+echo "COPY  ./templates /home/ubuntu/templates/" >> tempdir/Dockerfile
+echo "COPY  sample_app.py /home/ubuntu/" >> tempdir/Dockerfile
 echo "EXPOSE 80" >> tempdir/Dockerfile
+echo “CMD python /home/ubuntu/sample_app.py” >> tempdir/Dockerfile
 
 cd tempdir
 sudo docker build -t sampleapp .
 
 sudo docker stop samplerunning
 sudo docker rm samplerunning
-sudo docker run -t -d -p 8000:80 --name samplerunning sampleapp /bin/sh
+sudo docker run -t -d -p 8000:80 --name samplerunning sampleapp
 sudo docker ps -a
