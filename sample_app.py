e4944df408d7e51fad5310478b465f8fbce35721
Chase Marellan <chatbridge@gmail.com>
2019-10-25 02:44:59 +0000

    Added product and config pages

--- sample_app.py
+++ sample_app.py
@@ -4,6 +4,15 @@ from flask import render_template
 
 sample = Flask(__name__)
 
+DBHOST = 'NOT SET'
+def getdb():
+    import MySQLdb
+    global DBHOST
+    return MySQLdb.connect(host=DBHOST,
+                     user="devnetstudent",
+                     passwd="pass",
+                     db="products")
+
 @sample.route("/")
 def main():
     return render_template("index.html")
@@ -11,6 +20,31 @@ def main():
 @sample.route("/test")
 def test():
     return "You are calling me from "+request.remote_addr
+
+@sample.route("/config")
+def config():
+    return render_template("config.html")
+
+@sample.route("/config_action", methods=['GET', 'POST'])
+def config_action():
+    global DBHOST 
+    DBHOST = request.args.get('dbhost')
+    return "Saved database host as "+DBHOST
+
+@sample.route("/products")
+def products():
+    db = getdb()
+
+    cur = db.cursor()
+    cur.execute("select * from products")
+
+    output = ""
+    for row in cur.fetchall():
+        output = output +  str(row[0]) + " -- " + str(row[1]) + "<br />"
+
+    db.close()
+    return output
+
 
 if __name__ == "__main__":
     sample.run(host="0.0.0.0", port=80)
